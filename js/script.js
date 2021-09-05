552501347262e1ce0606d00c05f007f105f11222
bkj281 <66874987+bkj281@users.noreply.github.com>
2021-09-05 10:59:58 +0530

    Login Modal Added

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// window.onscroll = function() {myFunction()};
+// var navbar = document.getElementById("navbar");
+// var sticky = navbar.offsetTop;
+// function myFunction() {
+// 	if (window.scrollY >= sticky) {
+// 		navbar.classList.add("fixed-top")
+// 	} else {
+// 		navbar.classList.remove("fixed-top");
+// 	}
+// }
