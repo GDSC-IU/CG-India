a9b07ae2e09f64d2a6e38689fdad067f27b89cf3
bkj281 <66874987+bkj281@users.noreply.github.com>
2021-09-06 13:06:50 +0530

    Home Page 90% Complete

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
-// window.onscroll = function() {myFunction()};
-// var navbar = document.getElementById("navbar");
-// var sticky = navbar.offsetTop;
-// function myFunction() {
-// 	if (window.scrollY >= sticky) {
-// 		navbar.classList.add("fixed-top")
-// 	} else {
-// 		navbar.classList.remove("fixed-top");
-// 	}
-// }
+fitframe();
+window.onscroll = function() {fixnav()};
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+function fixnav() {
+	if (window.scrollY >= sticky) {
+		navbar.classList.add("fixed-top")
+	} else {
+		navbar.classList.remove("fixed-top");
+	}
+}
+
+window.onresize = function() {fitframe()};
+function fitframe() {
+	var src1 = './assets/pdf/CGI.pdf#zoom=136.5%';
+	var src2 = './assets/pdf/CGI.pdf#zoom=100%';
+	if (window.screen.availWidth <= 768) {
+		document.getElementById('iframe').src = src1;
+	}
+	else {
+		document.getElementById('iframe').src = src2;
+	}
+}
